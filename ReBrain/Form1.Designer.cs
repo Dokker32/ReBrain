f091bd228d08d33d71627fde2e0a387c323bc29c
Dokker32 <dokker_32@mail.ru>
2022-10-09 02:30:10 +0700

    Add project files.

---
+++ ReBrain/Form1.Designer.cs
@@ -0,0 +1,89 @@
+﻿
+namespace ReBrain
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.glControl2 = new OpenTK.GLControl();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(30, 314);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(140, 56);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Открыть файл";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // glControl2
+            // 
+            this.glControl2.BackColor = System.Drawing.Color.Black;
+            this.glControl2.Location = new System.Drawing.Point(21, 12);
+            this.glControl2.Name = "glControl2";
+            this.glControl2.Size = new System.Drawing.Size(447, 260);
+            this.glControl2.TabIndex = 1;
+            this.glControl2.VSync = false;
+            this.glControl2.Load += new System.EventHandler(this.glControl2_Load);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(474, 12);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(314, 260);
+            this.pictureBox1.TabIndex = 2;
+            this.pictureBox1.TabStop = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.glControl2);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "  ";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private OpenTK.GLControl glControl2;
+        private System.Windows.Forms.PictureBox pictureBox1;
+    }
+}
+
---
+++ ReBrain/Form1.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Drawing;
+using System.Threading;
+using System.Windows.Forms;
+using Dicom;
+using Dicom.Imaging;
+using OpenGL;
+using System.Collections.Generic;
+
+
+namespace ReBrain
+{
+    public partial class Form1 : Form
+    {
+        float ColorR, ColorG, ColorB;
+
+        private void glControl2_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog ofd = new OpenFileDialog();
+
+            if (ofd.ShowDialog() == DialogResult.OK)
+            {
+            }
+        }
+
+        public Form1()
+        {
+            InitializeComponent();
+
+            Color c = Form1.DefaultBackColor;
+            ColorR = (float)c.R / 255;
+            ColorG = (float)c.G / 255;
+            ColorB = (float)c.B / 255;
+
+        }
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            Gl.Initialize();
+
+            Gl.Viewport(0, 0, glControl2.Size.Width, glControl2.Size.Height);
+            Gl.LoadIdentity();
+            Gl.ClearColor(ColorR, ColorG, ColorB, 1.0f);
+
+        }
+    }
+}
